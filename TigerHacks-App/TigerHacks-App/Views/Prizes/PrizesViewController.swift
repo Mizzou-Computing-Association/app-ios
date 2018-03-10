f0f5c151ce56dc41703a2b08721fd5656fbd0279
Jonah Zukosky <jonah.zukosky@gmail.com>
2018-03-09 20:32:56 -0600

    Created general architecture

---
+++ TigerHacks-App/TigerHacks-App/Views/Prizes/PrizesViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PrizesViewController.swift
+//  TigerHacks-App
+//
+//  Created by Jonah Zukosky on 3/9/18.
+//  Copyright © 2018 Zukosky, Jonah. All rights reserved.
+//
+
+import UIKit
+
+class PrizesViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var prizeTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func changeSection(_ sender: UISegmentedControl) {
+        
+    }
+    
+}
---
+++ TigerHacks-App/TigerHacks-App/Views/TigerTalks/TigerTalksTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TigerTalksTableViewCell.swift
+//  TigerHacks-App
+//
+//  Created by Jonah Zukosky on 3/9/18.
+//  Copyright © 2018 Zukosky, Jonah. All rights reserved.
+//
+
+import UIKit
+
+class TigerTalksTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
