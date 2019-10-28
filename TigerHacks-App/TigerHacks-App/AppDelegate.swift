9123bdba4190bf24f00799210a4ec8a2d0ac6e06
Jonah Zukosky <jonah.zukosky@gmail.com>
2019-10-28 13:18:17 -0500

    favoriting

--- TigerHacks-App/TigerHacks-App/AppDelegate.swift
+++ TigerHacks-App/TigerHacks-App/AppDelegate.swift
@@ -130,7 +130,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+             }
         }
     }
 
